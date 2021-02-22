1d52de5a68dc60c8a73826027a5377b2baa08e77
Giacomo Agnetti <jackss.code@gmail.com>
2021-02-22 21:31:14 +0100

    Moved tests to a temporary file

--- server/Server.js
+++ server/Server.js
@@ -250,39 +250,3 @@ class SocketHandle {
 
 
 module.exports = { App }
-
-
-
-// console.log(parseSentence("P /test-number\n112"))
-// console.log(parseSentence("P /test- number\n112"))
-// console.log(parseSentence("p /test-number\n"))
-
-
-// function testLocationMatch(ref, tested) {
-//   const r = new Location(ref)
-//   const t = new Location(tested)
-//
-//   const { match, params } = r.matches(t)
-//
-//   console.log('testing:', {ref, tested})
-//
-//   if(match)
-//     console.log('match with params:', params)
-//   else
-//     console.log('no match')
-//
-//   console.log()
-// }
-//
-//
-//
-// testLocationMatch('/', '/')
-// testLocationMatch('/core/manifest', '/core/manifest')
-// testLocationMatch('/report/:id/temp', '/report/adx/temp')
-// testLocationMatch('/report/:id/:stat', '/report/1344/temp')
-//
-// testLocationMatch('/banana', '/banana/another')
-// testLocationMatch('/banana/another', '/banana')
-// testLocationMatch('/banana', '/not-banana')
-// testLocationMatch('/report/:id/:stat/boink', '/report/1344/temp')
-
